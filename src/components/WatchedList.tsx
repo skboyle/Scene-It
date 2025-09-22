0fd55958e136922148229f865b61a1f02b5dead9
Steve <skboylework@gmail.com>
2025-09-22 15:40:10 +0200

    add addtional movie details

--- src/components/WatchedList.tsx
+++ src/components/WatchedList.tsx
@@ -3,13 +3,14 @@ import type { WatchedMovie } from "@/types";
 interface WatchedListProps {
   watched: WatchedMovie[];
   onDeletedWatched: (id: string) => void;
+  onSelectMovie: (id: string) => void;
 }
 
-export function WatchedList({ watched, onDeletedWatched }: WatchedListProps) {
+export function WatchedList({ watched, onDeletedWatched, onSelectMovie }: WatchedListProps) {
   return (
     <ul className="list">
       {watched.map((movie) => (
-        <li key={movie.imdbID}>
+        <li key={movie.imdbID} onClick={() => onSelectMovie(movie.imdbID)}>
           <img src={movie.poster} alt={`${movie.title} poster`} />
           <h3>{movie.title}</h3>
           <div>
